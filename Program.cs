0fbc0ce912ace0e4141b0429320d06140f1c96df
Gilboot <ssenyonjogilbert@gmail.com>
2021-04-25 18:43:11 +0300

    Setup and configured SeedData so it can work

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie.Models;
+
 
 namespace RazorPagesMovie
 {
@@ -13,7 +16,24 @@ namespace RazorPagesMovie
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            // Seed data into the database
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try 
+                {
+                    SeedData.Initialize(services);
+                }
+                catch(Exception ex) 
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
