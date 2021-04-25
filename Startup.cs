7aa573ccf28346e79278471256bbd5fd51210400
Gilboot <ssenyonjogilbert@gmail.com>
2021-04-25 08:33:19 +0300

    Scaffolded CRUD pages. Added code in Startup.cs to use sqlite in development and sqlserver in production

--- Startup.cs
+++ Startup.cs
@@ -8,22 +8,38 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace RazorPagesMovie
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            // Use SqLite for development and SQLServer for production
+            if(Environment.IsDevelopment())
+            {
+                services.AddDbContext<RazorPagesMovieContext>(options =>
+                    options.UseSqlite(Configuration.GetConnectionString("RazorPagesMovieContext")));
+            }
+            else 
+            {
+                services.AddDbContext<RazorPagesMovieContext>(options => 
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("MovieContext")));
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
