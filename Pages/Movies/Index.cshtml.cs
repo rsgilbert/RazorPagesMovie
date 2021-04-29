99ca8a54cd1b5f65064428b869565d9e40fb139b
Gilboot <ssenyonjogilbert@gmail.com>
2021-04-29 04:44:09 +0300

    Added search functionality

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 
 
 namespace RazorPagesMovie.Pages_Movies
@@ -21,9 +23,21 @@ namespace RazorPagesMovie.Pages_Movies
 
         public IList<Movie> Movie { get;set; }
 
+        [BindProperty(SupportsGet=true)]
+        public string SearchString { get; set; }
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet=true)]
+        public String MovieGenre { get; set; }
+
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            var movies = from m in _context.Movie
+                            select m;
+            if(!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+            Movie = await movies.ToListAsync();
         }
     }
 }
